40f69eb7332a857ddaff193a8ae6828995d6d0a7
bqaddumi <bqaddumi@asaltech.com>
2019-06-05 18:15:26 +0300

    [*] init commit

---
+++ src/components/main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../styles/main.scss";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className={"container"}>
+        <h1>Main</h1>
+        <button className={"button"}>main</button>
+      </div>
+    );
+  }
+}
+
+export default Main;
